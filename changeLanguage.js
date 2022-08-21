10cae7975c192d48f23d8db58cacc927b219399c
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-20 23:35:35 -0300

    changing buttom 'view portfolio'

--- changeLanguage.js
+++ changeLanguage.js
@@ -2,7 +2,7 @@ function translateToEnglish(){
   
     document.getElementById('title').innerText = "About me 👪";
     document.getElementById('teacherH2').innerText = "ENGLISH TEACHER, SYSTEM ADMINISTRATOR, WEB DEVELOPER AND FATHER OF DAVID";
-    document.getElementById('portfolioView').innerText = "View my portfolio";
+   
     document.getElementById('hiParagraph').innerText = "Hi! I'm Luciano. For the past 3 years, I have been a system administrator by day and an English teacher by night. Being a teacher \
     has been a hobby for me.";
     document.getElementById('freeTimeParagrath').innerText = "In my spare time I like studying languages, spending time with my family and creating websites. Between the languages\
